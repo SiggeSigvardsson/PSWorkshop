2936cd7d0940224744b8204ce78107eeba5cc33f
Sigge Sigvardsson <sigge3000@hotmail.com>
2019-03-11 16:29:21 +0100

    test2

---
+++ Sigge-Temp.ps1
@@ -0,0 +1,29 @@
+git add (file.name)
+
+git commit -m "Info of commit" 
+
+Git log 
+
+#to get active files online from 
+git pull upstream master
+
+git push
+
+git commit
+
+git config --global user.name "Sigge Sigvardsson"
+git config --global user.email sigge3000@hotmail.com 
+git commit --amend --reset-author
+ 
+git config --edit
+
+git log
+
+git status
+
+#Upstream är fritext namn 
+git pull upstream master
+
+#Get full active info
+git checkout master 
+
